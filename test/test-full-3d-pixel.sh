926861d4a7fa4562410f15b6572f2a2563d5d838
fanrado <radofana@gmail.com>
2026-02-17 06:52:39 -0800

    bash script running all steps for  the FR calculations for pix

---
+++ test/test-full-3d-pixel.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+set -e
+export POCHOIR_STORE="${1:-store}"
+
+source helpers.sh
+
+# date
+
+##
+## Domains ##
+echo "=== Domains ==="
+do_domain () {
+    local name=$1 ; shift
+    local shape=$1; shift
+    local spacing=$1; shift
+
+    want domain/$name \
+         pochoir domain --domain domain/$name \
+         --shape=$shape --spacing $spacing
+}
+do_domain drift3d  44,44,1500  '0.1*mm'
+
+# fixme: these weight* identifiers need to split up for N planes.
+#do_domain weight2d 1092,2000   '0.1*mm'
+#do_domain weight3d 350,32,2000 '0.1*mm'
+
+
+## Initial/Boundary Value Arrays ##
+do_gen () {
+    local name=$1 ; shift
+    local geom=$1; shift
+    local gen="pcb_drift_pixel_with_grid"
+    local cfg="example_gen_pcb_drift_pixel_with_grid.json"
+
+    want initial/$name \
+         pochoir gen --generator $gen --domain domain/$name \
+         --initial initial/$name --boundary boundary/$name \
+         $cfg
+
+}
+do_gen drift3d quarter
+# do_gen weight2d 2D 
+# do_gen weight3d 3D
+
+echo "=== Fields ==="
+## Fields
+do_fdm () {
+    local name=$1 ; shift
+    local nepochs=$1 ; shift
+    local epoch=$1 ; shift
+    local prec=$1 ; shift
+    local edges=$1 ; shift
+
+    want potential/$name \
+         pochoir fdm \
+         --nepochs $nepochs --epoch $epoch --precision $prec \
+         --edges $edges \
+         --engine torch \
+         --initial initial/$name --boundary boundary/$name \
+         --potential potential/$name \
+         --increment increment/$name
+}
+do_fdm drift3d  10      1300000      0.00000002     per,per,fix #130,000,000
+# do_fdm weight2d 1      1200      0.00000002   fix,fix #1250000
+
+##
+echo "=== Weighting potentials ==="
+## Weighting potentials
+## Domains ##
+do_domain () {
+    local name=$1 ; shift
+    local shape=$1; shift
+    local spacing=$1; shift
+
+    want domain/$name \
+         pochoir domain --domain domain/$name \
+         --shape=$shape --spacing $spacing
+}
+
+do_domain weight3d 396,396,1500 '0.1*mm' #220,220,1500 '0.1*mm'
+
+
+## Initial/Boundary Value Arrays ##
+do_gen () {
+    local name=$1 ; shift
+    local geom=$1; shift
+    local gen="pcb_pixel_with_grid"
+    local cfg="example_gen_pixel_with_grid.json"
+
+    want initial/$name \
+         pochoir gen --generator $gen --domain domain/$name \
+         --initial initial/$name --boundary boundary/$name \
+         $cfg
+
+}
+do_gen weight3d 3D
+
+## Fields
+do_fdm () {
+    local name=$1 ; shift
+    local nepochs=$1 ; shift
+    local epoch=$1 ; shift
+    local prec=$1 ; shift
+    local edges=$1 ; shift
+
+    want potential/$name \
+         pochoir fdm \
+         --nepochs $nepochs --epoch $epoch --precision $prec \
+         --edges $edges \
+	 --engine torch \
+         --initial initial/$name --boundary boundary/$name \
+         --potential potential/$name \
+         --increment increment/$name
+}
+do_fdm weight3d 1      500000      0.00000002   fix,fix,fix #5000000 ### try 100 epochs, and more steps per epoch
+
+#
+echo "=== Velocities ==="
+## Velocities
+want velocity/drift3d \
+     pochoir velo --temperature '87.0*K' \
+     --potential potential/drift3d \
+     --velocity velocity/drift3d
+#
+
+## Need to be run separately
+echo "=== Paths ==="
+## Paths
+want starts/drift3d \
+    pochoir starts --starts starts/drift3d \
+    -m yes \
+    '0.1*mm,0.1*mm,148*mm'
+
+#rm -r /Users/sergey/Desktop/ICARUS/LArStand/pochoir/test/store/paths
+
+want paths/drift3d_tight \
+     pochoir drift --starts starts/drift3d \
+     --velocity velocity/drift3d \
+     --paths paths/drift3d_tight '0*us,320*us,0.05*us'
+
+
+##
+echo "=== Induced currents ==="
+## Induced currents
+want current/induced_current \
+     pochoir induce-pixel --weighting potential/weight3d \
+     --paths paths/drift3d_tight \
+     --output current/induced_current \
+     --npixels 2.0
